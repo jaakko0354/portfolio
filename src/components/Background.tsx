1597d7ca12aa0d10baf6d8232fad806dafd295dd
Jaakko Miettinen <jaakkomiettinen@Jaakko-Kannettava-tietokone.local>
2024-05-12 14:33:57 +0300

    Added some hatch styles -not done yet

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef, useState } from 'react';
-import '../styles/Background.css';
-import { Avatar } from 'antd';
-import cvkuva from '../assets/cv-kuva.jpg';
-import Projects from './Projects';
-import { DoubleRightOutlined,} from '@ant-design/icons';
+import { useEffect, useRef, useState } from "react";
+import "../styles/Background.css";
+import { Avatar } from "antd";
+import cvkuva from "../assets/cv-kuva.jpg";
+import Projects from "./Projects";
+import { DoubleRightOutlined } from "@ant-design/icons";
 
 export default function Background() {
   const projectsRef = useRef<HTMLDivElement>(null);
@@ -11,9 +11,9 @@ export default function Background() {
   const [scrollY, setScrollY] = useState(0);
   const [showAvatar, setShowAvatar] = useState(true);
   const [showProjecs, setShowProjects] = useState(false);
-  
+  const [isActive, setIsActive] = useState(false);
+
   const [hasScrolledToProjects, setHasScrolledToProjects] = useState(false);
-  
 
   //https://stackoverflow.com/a/71682214
   useEffect(() => {
@@ -26,7 +26,7 @@ export default function Background() {
         setShowProjects(false);
       }
       console.log(window.scrollY);
-      if ( 150 < window.scrollY && window.scrollY < 660) {
+      if (150 < window.scrollY && window.scrollY < 660) {
         if (!hasScrolledToProjects) {
           //focusProjects();
           setHasScrolledToProjects(true);
@@ -34,12 +34,10 @@ export default function Background() {
       } else {
         setHasScrolledToProjects(false);
       }
-
     };
 
-    
     handleScroll();
-    
+
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
@@ -47,68 +45,112 @@ export default function Background() {
   }, [hasScrolledToProjects]);
 
   const avatarClick = () => {
-    const avatar = document.querySelector('.profile-picture');
-    avatar?.classList.toggle('active');
-    const info = document.querySelector('.info-container');
-    info?.classList.toggle('active');
-  }
-  
+    setIsActive(!isActive);
+  };
 
   /* const focusProjects = () => {
     document.documentElement.scrollTo({top: 530, behavior:'smooth'});
   } */
-
 
   const avatarOpacity = Math.max(0, Math.min(1, (250 - scrollY) / 250));
 
   return (
     <div className="background-color">
       {showAvatar ? (
-          <div className='avatar-container'>
-            <Avatar size={300} src={cvkuva} className="profile-picture" style={{ opacity: avatarOpacity }} onClick={() => avatarClick()}/>
-            <div className="info-container" onClick={() => avatarClick()} style={{ opacity: avatarOpacity }}>
-              <div className='info-box'/>
-              <div className='info-ball'/>
-              <p className='info-text'>About me</p></div>
-            </div>
-        ) : (
-          <div className="avatar-placeholder" />
-          )}
-        
-      <div className="background-triangle" style={{ transform: `translateY(-${scrollY * 0.4}px)`, zIndex:2}}/>
-      <div className="background-square" style={{ transform: `translateY(-${scrollY * 0.4}px)`, zIndex:3}}>
-        <DoubleRightOutlined 
-        rotate={90}
-        style={{ fontSize:'50px', color:'#269d3e',}}
-        className='down-arrow' onClick={() => {document.documentElement.scrollTo({top: 520, behavior:'smooth'})}}
+        <div className="avatar-container">
+          <div
+            className={`profile-picture ${isActive ? "active" : ""}`}
+            style={{ opacity: avatarOpacity }}
+            onClick={() => avatarClick()}
+          >
+            <Avatar size={300} src={cvkuva} className="front" />
+            <div className="back " />
+          </div>
+          <div
+            className="info-box"
+            onClick={() => avatarClick()}
+            style={{ opacity: avatarOpacity }}
+          />
+          <div
+            className="info-ball"
+            onClick={() => avatarClick()}
+            style={{ opacity: avatarOpacity }}
+          />
+          <p
+            className="info-text"
+            onClick={() => avatarClick()}
+            style={{ opacity: avatarOpacity }}
+          >
+            About me
+          </p>
+        </div>
+      ) : (
+        <div className="avatar-placeholder" />
+      )}
+
+      <div
+        className="background-triangle"
+        style={{ transform: `translateY(-${scrollY * 0.4}px)`, zIndex: 2 }}
+      />
+      <div
+        className="background-square"
+        style={{ transform: `translateY(-${scrollY * 0.4}px)`, zIndex: 3 }}
+      >
+        <DoubleRightOutlined
+          rotate={90}
+          style={{ fontSize: "50px", color: "#269d3e" }}
+          className="down-arrow"
+          onClick={() => {
+            document.documentElement.scrollTo({ top: 520, behavior: "smooth" });
+          }}
         />
-        <div className="background-bottom-triangle"/>
+        <div className="background-bottom-triangle" />
       </div>
 
       {showProjecs ? (
-        <Projects ref={projectsRef} scrollY={scrollY}/>
-      ) :(
-        <div className='projects-placeholder'/>
-      )
-      }
-      <div className="background-triangle" style={{ transform: `translateY(-${scrollY * 0.8}px)` }} />
-      <div className="background-square" style={{ transform: `translateY(-${scrollY * 0.8}px)`}}>
-        <DoubleRightOutlined 
-        rotate={90}
-        style={{ fontSize:'50px', color:'#269d3e',}}
-        className='down-arrow' onClick={() => {document.documentElement.scrollTo({top: 960, behavior:'smooth'})}}
+        <Projects ref={projectsRef} scrollY={scrollY} />
+      ) : (
+        <div className="projects-placeholder" />
+      )}
+      <div
+        className="background-triangle"
+        style={{ transform: `translateY(-${scrollY * 0.8}px)` }}
+      />
+      <div
+        className="background-square"
+        style={{ transform: `translateY(-${scrollY * 0.8}px)` }}
+      >
+        <DoubleRightOutlined
+          rotate={90}
+          style={{ fontSize: "50px", color: "#269d3e" }}
+          className="down-arrow"
+          onClick={() => {
+            document.documentElement.scrollTo({ top: 960, behavior: "smooth" });
+          }}
         />
-        <div className="background-bottom-triangle"/>
+        <div className="background-bottom-triangle" />
       </div>
-      <div style={{height:'450px'}}></div>
-      <div className="background-triangle" style={{ transform: `translateY(-${scrollY * 0.9}px)` }} />
-      <div className="background-square" style={{ transform: `translateY(-${scrollY * 0.9}px)` }}>
-        <DoubleRightOutlined 
-        rotate={90}
-        style={{ fontSize:'50px', color:'#269d3e',}}
-        className='down-arrow' onClick={() => {document.documentElement.scrollTo({top: 1300, behavior:'smooth'})}}
+      <div style={{ height: "450px" }}></div>
+      <div
+        className="background-triangle"
+        style={{ transform: `translateY(-${scrollY * 0.9}px)` }}
+      />
+      <div
+        className="background-square"
+        style={{ transform: `translateY(-${scrollY * 0.9}px)` }}
+      >
+        <DoubleRightOutlined
+          rotate={90}
+          style={{ fontSize: "50px", color: "#269d3e" }}
+          className="down-arrow"
+          onClick={() => {
+            document.documentElement.scrollTo({
+              top: 1300,
+              behavior: "smooth",
+            });
+          }}
         />
-        <div className="background-bottom-triangle"/>
+        <div className="background-bottom-triangle" />
       </div>
     </div>
   );
